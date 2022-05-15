25559fcad1e7148427327447e4e09011157bafc6
j0dev <rig0408@naver.com>
2022-05-15 20:34:14 +0900

    set spring security  - set google oauth

---
+++ src/main/java/com/j0dev/book/springboot/domain/user/Role.java
@@ -0,0 +1,16 @@
+package com.j0dev.book.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "게스트"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String label;
+
+}
